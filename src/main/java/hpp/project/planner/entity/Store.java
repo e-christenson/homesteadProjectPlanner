a4eeb4b77f15a2481da10559a0e01f9c7ac9c766
e-christenson <erikchristenson@gmail.com>
2021-12-04 22:00:31 -0600

    working on setting up a db / class / hibernate / DAO test for STORE entity

--- src/main/java/hpp/project/planner/entity/Store.java
+++ src/main/java/hpp/project/planner/entity/Store.java
@@ -1,4 +1,69 @@
 package hpp.project.planner.entity;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+
+ */
+
+@Entity(name = "Store")
+@Table(name = "store")
 public class Store {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    @ManyToOne
+    private Project project;
+
+    private String item;
+
+    public Store(int id, Project project, String item) {
+        this.id = id;
+        this.project = project;
+        this.item = item;
+    }
+
+    public Store() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", project=" + project +
+                ", item='" + item + '\'' +
+                '}';
+    }
 }
---
+++ src/test/java/hpp/project/planner/persistence/StoreTest.java
@@ -0,0 +1,52 @@
+package hpp.project.planner.persistence;
+import hpp.project.planner.entity.Project;
+import hpp.project.planner.entity.Store;
+import hpp.project.planner.entity.User;
+import hpp.project.planner.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The type store dao test.
+ */
+
+public class StoreTest {
+
+    GenericDao dao;
+    GenericDao projectDao;
+    List<Project> projects;
+
+
+    /**
+     * Sets up.
+     */
+    @BeforeEach
+    void setUp() {
+        dao = new GenericDao(Store.class);
+        projectDao = new GenericDao(Project.class);
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
+
+
+    @Test
+    void insertNewStoreItem() {
+        projects =  projectDao.findByPropertyEqual("id",1);
+        Store newStoreInsert = new Store(0,projects.get(0),"belts");
+        int projectNum = dao.insert(newStoreInsert);
+
+        Store pullFromDB = (Store) dao.getById(projectNum);
+
+        assertEquals("belts", pullFromDB.getItem());
+
+
+
+    }
+
+}
