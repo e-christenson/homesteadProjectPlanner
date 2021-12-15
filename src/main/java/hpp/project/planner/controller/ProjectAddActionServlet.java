c8d4418545d89cd5c527f256400c145cf7e321f1
e-christenson <erikchristenson@gmail.com>
2021-12-15 16:13:26 -0600

    jdoc in logout/project servlets

--- src/main/java/hpp/project/planner/controller/ProjectAddActionServlet.java
+++ src/main/java/hpp/project/planner/controller/ProjectAddActionServlet.java
@@ -16,16 +16,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.List;
 
 /**
- *  Homestead Project Planer
- *  add user servlet
- *@author    EChristenson
+ * Homestead Project Planer
+ * add user servlet
+ *
+ * @author EChristenson
  */
 @WebServlet(
         name = "HPPaddProject",
-        urlPatterns = { "/HPPaddProject" }
+        urlPatterns = {"/HPPaddProject"}
 )
 public class ProjectAddActionServlet extends HttpServlet {
 
@@ -37,105 +37,105 @@ public class ProjectAddActionServlet extends HttpServlet {
     LocalDate date;
 
 
-
     /**
+     * Handles HTTP GET requests.
      *
-     *  Handles HTTP GET requests.
-     *
-     *@param  request                   the HttpServletRequest object
-     *@param  response                   the HttpServletResponse object
-     *@exception  ServletException  if there is a Servlet failure
-     *@exception  IOException       if there is an IO failure
+     * @param request  the HttpServletRequest object
+     * @param response the HttpServletResponse object
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
      */
 
 
-    public void doPost (HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         loggedInUser = (User) request.getSession().getAttribute("cognitoUser");
-        //info in from web form
+        //info in from web form loaded into variables
 
-    String helper = request.getParameter("helper");
-    String store = request.getParameter("store");
-    String storeFlag = null;
-        if (store.length() > 1){storeFlag = "y";}
+        String helper = request.getParameter("helper");
+        String store = request.getParameter("store");
+        String storeFlag = null;
+        if (store.length() > 1) {
+            storeFlag = "y";
+        }
 
-        String  projectName = request.getParameter("projectName");
-        String  Wday = request.getParameter("Wday");
-        String  Sday = request.getParameter("Sday");
-        String  in_out = request.getParameter("in_out");
-        String  hot_cold = request.getParameter("hot_cold");
-        String  windy = request.getParameter("windy");
-        String editProjectID =request.getParameter("projectEditID");
+        String projectName = request.getParameter("projectName");
+        String Wday = request.getParameter("Wday");
+        String Sday = request.getParameter("Sday");
+        String in_out = request.getParameter("in_out");
+        String hot_cold = request.getParameter("hot_cold");
+        String windy = request.getParameter("windy");
+
+        //this servlet can add AND edit projects so this could be null
+        String editProjectID = request.getParameter("projectEditID");
+
         //need this above in int, but could be null
-        if (editProjectID == null){
-            editProjectID="0";
+        if (editProjectID == null) {
+            //new project set at zero 0
+            editProjectID = "0";
             date = LocalDate.now();
         } else {
+            //if we edit project we need its original data to keep that
             date = LocalDate.parse(request.getParameter("date"));
         }
         int editOrUpdateID = Integer.parseInt(editProjectID);
 
 
+        logger.info("STORE ITEM LIST LENGTH " + store.length());
 
-
-        //is this a new project or an edit.  hew=0 project id bc it auto indexes that column
-       // if (editProjectID == null){editProjectID="0";}
-    //int insertOrEdit = Integer.parseInt(editProjectID);
-
-        //setup DB flag y or n based on if store items exist
-
-        logger.info("STORE ITEM LIST LENGTH "+store.length());
-
-
-
-        Project newProject = new Project( editOrUpdateID, loggedInUser, projectName, date, Wday, Sday, helper, storeFlag, in_out, hot_cold, windy,0);
+        //setting up new project or edited project (both are a project)
+        Project newProject = new Project(editOrUpdateID, loggedInUser, projectName, date, Wday, Sday, helper, storeFlag, in_out, hot_cold, windy, 0);
         loggedInUser.addProject(newProject);
-        logger.info("is newProject set???  "+newProject);
+        logger.info("is newProject set???  " + newProject);
 
-        //new projects will have null editProjectID
-        //newe gets inserted, existing gets updated
-        //updated projects set newProjectID = for store list update/edit
-if (editOrUpdateID == 0){
-     editOrUpdateID = dao.insert(newProject);
-    } else {
- dao.saveOrUpdate(newProject);
+        // here we decide if we are inserting new or doing an edit
 
-}
+        if (editOrUpdateID == 0) {
+            //a new project
+            editOrUpdateID = dao.insert(newProject);
+        } else {
+            dao.saveOrUpdate(newProject);
 
-
+        }
 
 
-       //int newProjectID = dao.insert(newProject);
 
+        //if store contains items, now we have project ID we can insert
+        //store items into their table
 
-        //if store contains items, now we have project ID we can insert them
-        if (store.length() >1 ){
-            setStoreStringToDB(store,editOrUpdateID);
+        if (store.length() > 1) {
+            setStoreStringToDB(store, editOrUpdateID);
         }
 
 
         String url = "/index";
-        request.setAttribute("url",url);
+        request.setAttribute("url", url);
         RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-        dispatcher.forward(request,response);
+        dispatcher.forward(request, response);
     }
 
     public void init() throws ServletException {
     }
 
-    private void setStoreStringToDB(String storeString, int projectID){
+
+    /**
+     * this method takes a string, splits it into words by (space)
+     * then each word is put into a store DB table
+     * @param storeString
+     * @param projectID
+     */
+    private void setStoreStringToDB(String storeString, int projectID) {
         GenericDao dao = new GenericDao(Store.class);
         String[] indItemsStore = storeString.split("\\s+");
 
-        for (String item: indItemsStore ) {
-            Store newInsert = new Store(0,projectID,loggedInUser.getId(),item);
-           // newProject.addStore(newInsert);
+        for (String item : indItemsStore) {
+            Store newInsert = new Store(0, projectID, loggedInUser.getId(), item);
+            // newProject.addStore(newInsert);
             dao.insert(newInsert);
 
         }
 
     }
-
 
 
 }
--- src/main/java/hpp/project/planner/controller/logoutActionServlet.java
+++ src/main/java/hpp/project/planner/controller/logoutActionServlet.java
@@ -1,9 +1,5 @@
 package hpp.project.planner.controller;
 
-import hpp.project.planner.entity.Store;
-import hpp.project.planner.entity.User;
-import hpp.project.planner.persistence.GenericDao;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,50 +9,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
 
 
 /**
- *  unit4 lab2
- *
- *@author    EChristenson
+ * used to log a user out
+ * it takes the session and removes it
+ * @author EChristenson
  */
 @WebServlet(
         name = "logout",
-        urlPatterns = { "/logout" }
+        urlPatterns = {"/logout"}
 )
 public class logoutActionServlet extends HttpServlet {
 
 
-
     /**
+     * Handles HTTP GET requests.
      *
-     *  Handles HTTP GET requests.
-     *
-     *@param  request                   the HttpServletRequest object
-     *@param  response                   the HttpServletResponse object
-     *@exception  ServletException  if there is a Servlet failure
-     *@exception  IOException       if there is an IO failure
+     * @param request  the HttpServletRequest object
+     * @param response the HttpServletResponse object
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
-
-
         request.getSession().invalidate();
-
-
 
         String url = "/logout.jsp";
 
-        RequestDispatcher  dispatcher = getServletContext().getRequestDispatcher(url);
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
-
     }
 
+
+    /**
+     *  init method
+     * @throws ServletException
+     */
     public void init() throws ServletException {
-        ServletContext sc = getServletContext();
-        log("inside **init** method printing this msg");
 
 
     }
